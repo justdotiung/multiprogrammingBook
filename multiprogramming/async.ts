6d442459ea515791f50de78eae7488b10a166a92
zeta <2014brand@naver.com>
2025-05-23 23:10:12 +0900

    4장 공부중

--- multiprogramming/async.ts
+++ multiprogramming/async.ts
@@ -1,6 +1,11 @@
+export = {};
 
 function delay<T>(time:number, value: T): Promise<T> {
-    return new Promise((r)=> setTimeout(r, time, value));
+    return new Promise((r)=> setTimeout(r, time, value))
+}
+
+function delayReject(time:number, value: unknown): Promise<never> {
+    return new Promise((_,j)=> setTimeout(j, time, value));
 }
 
 
@@ -17,9 +22,108 @@ function getFriends(): Promise<User[]> {
     return delay(getRandomValue(60, 6000), [{name: "Marty"}, {name: "black"}, {name: "yellow"}])
 }
 
-(async function test3() {
-    const result = await Promise.race([getFriends(), delay(5000, "timeout")]);
+function toggleLoadingIndicator(isShow:boolean):void {
+    if(isShow) {
+        console.log('append loading...')
+    }else {
+        console.log('remove loading...')
+    }
+}
+
+async function renderFriendsPicker():Promise<void> {
+    const friendsPromise = getFriends();
+
+    const result = await Promise.race([friendsPromise, delay(100, 'isShow')])
+
+    if(result === 'isShow') {
+        toggleLoadingIndicator(true);
+        await friendsPromise;
+        toggleLoadingIndicator(false)
+    }
+
+    const friends = await friendsPromise;
+    console.log("rendering", friends.map(({name}) => `<li>${name}</li>`))
+
+}
+
+
+
+type File = {
+    name: string;
+    body: string;
+    size: number;
+}
+
+function getFile(name:string, size = 1000): Promise<File> {
+    return delay(size, {name, body: '...', size})
+}
+
+const settlePromise = <T>(promise: Promise<T>) => promise.then(value => ({status: 'fulfilled', value})).catch(reason => ({status:'rejected', reason}))
+
+async function getFiles() {
+    try {
+        const files = await Promise.all([
+            getFile("img.png", 1500),
+            getFile("book.pdf", 1000),
+            getFile("index.html", 500),
+            delayReject(500,'fail....')
+            
+        ])
+        console.log(files)
+    } catch(e) {
+        console.error(e)
+    }
+}
+
+
+
+async function getFiles2() {
+    // const files = await Promise.allSettled([
+    //     getFile("img.png", 1500),
+    //     getFile("book.pdf", 1000),
+    //     getFile("index.html", 500),
+    //     Promise.reject("fail...")
+    // ])
     
+    // const files = await Promise.allSettled([
+    //     getFile("img.png", 1500),
+    //     getFile("book.pdf", 1000),
+    //     getFile("index.html", 500),
+    //     Promise.reject("fail...")
+    // ].map(settlePromise))
+    try {
+
+        const files = await Promise.any([
+            getFile("img.png", 1500),
+            getFile("book.pdf", 1000),
+            getFile("index.html", 500),
+            Promise.reject("fail...")
+        ])
+        
+        console.log(files);
+        
+        const files2 = await Promise.any([
+            delayReject(200, 'fail...'),
+            delayReject(300, 'fail...'),
+        ])
+        
+        console.log(files2)
+    }catch(e) {
+        console.error(e)
+    }
+}
+
+
+
+(function main() {
+    // renderFriendsPicker();
+    getFiles2()
+
+})()
+
+async function test3 (){
+    const result = await Promise.race([getFriends(), delay(5000, "timeout")]);
+
 
     if(result === 'timeout') {
         console.log('network error')
@@ -27,8 +131,8 @@ function getFriends(): Promise<User[]> {
         const friends = result as User[];
         console.log("rendering", friends.map(({name}) => `<li>${name}</li>`))
     }
-})()
 
+}
 function test() {
     console.time("test");
 
